2032919cc19fdac210b4c0e70c1427b4bd65730a
ivogluhchev <ivogluhchev@gmail.com>
2022-09-23 17:15:21 +0300

    FB Login

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -72,4 +72,58 @@ export default class UserStore {
         if (this.user) this.user.displayName = name;
     }
 
+    facebookLogin = () => {
+        window.FB.login(response => {
+            console.log(response);
+        }, {scope: 'ublic_profile,email'})
+    }
+
+    // facebookLogin = () => {
+    //     this.fbLoading = true;
+    //     const apiLogin = (accessToken: string) => {
+    //         agent.Account.fbLogin(accessToken).then(user => {
+    //             store.commonStore.setToken(user.token);
+    //             this.startRefreshTokenTimer(user);
+    //             runInAction(() => {
+    //                 this.user = user;
+    //                 this.fbLoading = false;
+    //             })
+    //             history.push('/activities');
+    //         }).catch(error => {
+    //             console.log(error);
+    //             runInAction(() => this.fbLoading = false);
+    //         })
+    //     }
+    //     if (this.fbAccessToken) {
+    //         apiLogin(this.fbAccessToken);
+    //     } else {
+    //         window.FB.login(response => {
+    //             apiLogin(response.authResponse.accessToken);
+    //         }, {scope: 'public_profile,email'})
+    //     }
+    // }
+
+    // refreshToken = async () => {
+    //     this.stopRefreshTokenTimer();
+    //     try {
+    //         const user = await agent.Account.refreshToken();
+    //         runInAction(() => this.user = user);
+    //         store.commonStore.setToken(user.token);
+    //         this.startRefreshTokenTimer(user);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // private startRefreshTokenTimer(user: User) {
+    //     const jwtToken = JSON.parse(atob(user.token.split('.')[1]));
+    //     const expires = new Date(jwtToken.exp * 1000);
+    //     const timeout = expires.getTime() - Date.now() - (60 * 1000);
+    //     this.refreshTokenTimeout = setTimeout(this.refreshToken, timeout);
+    // }
+
+    // private stopRefreshTokenTimer() {
+    //     clearTimeout(this.refreshTokenTimeout);
+    // }
+
 }
